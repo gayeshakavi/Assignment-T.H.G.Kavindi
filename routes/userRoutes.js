0cf6e3688002bb151971b36eab861d9577255767
Gayesha Kavindi <kavindi12416@usci.ruh.ac.lk>
2025-06-27 23:59:55 +0530

    Modified UserRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,15 +3,34 @@ const router = express.Router();
 const User = require("../models/User");
 
 router.post("/register", async (req, res) => {
-  const user = new User(req.body);
-  await user.save();
-  res.send("User registered");
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res.status(400).send("Username and password are required");
+    }
+
+    const userExists = await User.findOne({ username });
+    if (userExists) return res.status(409).send("Username already exists");
+
+    const user = new User({ username, password });
+    await user.save();
+    res.send("User registered");
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
 router.post("/login", async (req, res) => {
-  const user = await User.findOne(req.body);
-  if (user) res.send("Login success");
-  else res.send("Login failed");
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username, password });
+    if (user) res.send("Login success");
+    else res.status(401).send("Login failed");
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
 module.exports = router;
